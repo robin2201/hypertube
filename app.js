31c2e479d7282550ee4a269d9d262449e385713c
Robin BERTHIER <rberthie@e1r13p9.42.fr>
2017-04-23 17:48:52 +0200

    Add

--- app.js
+++ app.js
@@ -1,53 +1,61 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const compress = require('compression')
-const index = require('./routes/index');
-const users = require('./routes/users');
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const index = require('./routes/index')
+const users = require('./routes/users')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
 
-const app = express();
+const sess = session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}
+})
+
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(sess)
 
-app.use(compress({
-    threshold: 1400,
-    level: 4,
-    memLevel: 3
-}));
 
-app.use('/', index);
-app.use('/users', users);
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', index)
+app.use('/users', users)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  let err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('index', {
+      message:next.message,
+      type:next.type
+  })
+})
 
-
-module.exports = app;
+module.exports = app
