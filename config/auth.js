d7e9f0c70a0d595fd5930e064dc8b34b18e68b69
rberthie <rberthie@student.42.fr>
2017-07-12 10:23:51 +0200

    remove keys

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 
 
 const facebookAuth = {
-    'clientID'      : '403301276718511',
-    'clientSecret'  : '8048a8c4235b0d818c3f729fb56594e8',
+    'clientID'      : '',
+    'clientSecret'  : '',
     'callbackURL'   : 'http://localhost:3000/users/facebook/cb'
 }
 
@@ -13,14 +13,14 @@ const twitterAuth = {
 }
 
 const googleAuth = {
-    key:`409337954243-qlp9l41la5a1m7eqd31n8kqn8ntqcjlm.apps.googleusercontent.com`
+    key:``
 
 }
 
 const Auth42 = {
     grant_type: 'authorization_code',
-    client_id: 'bfa18ca1d008f4f16d51aa04f4dd4bf84924230c45c0f3987c94094c0f1eaaf1',
-    client_secret: '121cdf8ae98045e53e40e41f5f6688ed4808ac262e3ec52e04f693d7a293ebda',
+    client_id: '',
+    client_secret: '',
     code: ``,
     redirect_uri: 'http://localhost:3000/users/oauth/42'
 }
